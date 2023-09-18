50a8fde16a34216482895e6266b38d45a50b0b9b
Houda08 <124877070+Houda08@users.noreply.github.com>
2023-09-18 16:24:04 +0200

    Update lib.rs

--- exercice2/src/lib.rs
+++ exercice2/src/lib.rs
@@ -1,93 +1,109 @@
 pub fn compute_string(_i: usize) -> String {
-  String::new()
+
+  if _i%3 == 0 && _i%5 ==0{
+      return "FizzBuzz".to_string();
+  }  
+  else if _i%3 == 0 {
+      return "Fizz".to_string();
+  }
+  else if _i%5 ==0 {
+      return "Buzz".to_string();
+  }
+  else {
+      return _i.to_string();
+  }
+   
+  
 }
 
 pub fn compute_list(_i: usize) -> Vec<String> {
-  vec!()
+  let mut res = Vec::new();
+  for i in 0 .. _i {
+      res.push(compute_string(i+1));
+  }
+  res
 }
 
 #[cfg(test)]
 mod tests {
   use super::*;
 
-  #[ignore]
   #[test]
   fn test_compute_string_should_return_1_when_1() {
     let result = compute_string(1);
     assert_eq!("1", result);
   }
 
-  #[ignore]
   #[test]
   fn test_compute_string_should_return_2_when_2() {
     let result = compute_string(2);
     assert_eq!("2", result);
   }
 
-  #[ignore]
+  
   #[test]
   fn test_compute_string_should_return_fizz_when_3() {
     let result = compute_string(3);
     assert_eq!("Fizz", result);
   }
 
-  #[ignore]
+  
   #[test]
   fn test_compute_string_should_return_4_when_4() {
     let result = compute_string(4);
     assert_eq!("4", result);
   }
 
-  #[ignore]
+  
   #[test]
   fn test_compute_string_should_return_buzz_when_5() {
     let result = compute_string(5);
     assert_eq!("Buzz", result);
   }
 
-  #[ignore]
+  
   #[test]
   fn test_compute_string_should_return_fizz_when_6() {
     let result = compute_string(6);
     assert_eq!("Fizz", result);
   }
 
-  #[ignore]
+  
   #[test]
   fn test_compute_string_should_return_fizz_when_9() {
     let result = compute_string(9);
     assert_eq!("Fizz", result);
   }
 
-  #[ignore]
+  
   #[test]
   fn test_compute_string_should_return_buzz_when_10() {
     let result = compute_string(10);
     assert_eq!("Buzz", result);
   }
 
-  #[ignore]
+  
   #[test]
   fn test_compute_string_should_return_fizz_buzz_when_15() {
     let result = compute_string(15);
     assert_eq!("FizzBuzz", result);
   }
 
-  #[ignore]
+  
   #[test]
   fn test_compute_list_should_return_a_sequence_of_5_elements_when_5() {
     let result = compute_list(5);
     assert_eq!(result.len(), 5)
   }
 
-  #[ignore]
+  
   #[test]
   fn test_compute_list_should_return_a_sequence_of_5_fizz_buzz_elements_when_5() {
     let result = compute_list(5);
     assert_eq!(result, vec!("1", "2", "Fizz", "4", "Buzz"));
   }
 
-  #[ignore]
+  
   #[test]
   fn test_compute_list_should_return_a_sequence_of_15_fizz_buzz_elements_when_15() {
     let result = compute_list(15);
